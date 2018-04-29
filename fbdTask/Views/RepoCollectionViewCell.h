12cd0af47e3acbf15ed6910d564f1d239dbeda79
Salem Mohammed <salem.muhmed@gmail.com>
2018-04-29 17:50:43 +0200

    create repo cell class

---
+++ fbdTask/Views/RepoCollectionViewCell.h
@@ -0,0 +1,18 @@
+//
+//  RepoCollectionViewCell.h
+//  fbdTask
+//
+//  Created by Salem Mohamed on 4/29/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepoCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *repoDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ownerLoginLabel;
+
+
+@end
---
+++ fbdTask/Views/RepoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RepoCollectionViewCell.m
+//  fbdTask
+//
+//  Created by Salem Mohamed on 4/29/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+#import "RepoCollectionViewCell.h"
+
+@implementation RepoCollectionViewCell
+
+@end

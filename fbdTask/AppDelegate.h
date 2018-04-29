aa59f5289ffe347861d68219bbeb0643644afa72
Salem Mohammed <salem.muhmed@gmail.com>
2018-04-29 16:48:48 +0200

    project creation

---
+++ fbdTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbdTask
+//
+//  Created by Salem Mohamed on 4/29/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fbdTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fbdTask
+//
+//  Created by Salem Mohamed on 4/29/18.
+//  Copyright © 2018 Salem Mohammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

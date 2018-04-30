5ad47f0567cd11b1636eac92c89604b2d4b7455b
Salem Mohammed <salem.muhmed@gmail.com>
2018-04-30 19:31:32 +0200

    cell repo width to equal the size of the custom content view

--- fbdTask/Views/RepoCollectionViewCell.m
+++ fbdTask/Views/RepoCollectionViewCell.m
@@ -12,6 +12,7 @@
 
 -(void) awakeFromNib {
     [super awakeFromNib];
+    // Set the cell's custom content view to the screen width and that will force both the cell and the content view to be within the screen width.
     _contentViewWidthConstraint.constant = UIScreen.mainScreen.bounds.size.width - 20 ;
 }
 
